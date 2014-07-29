604b62f162a1a43ea1599739892fe79d5b3b27db
Joel Robinson <laconic75@gmail.com>
2014-07-29 18:12:26 -0500

    Added Window class, Regression :ESC no longer exits

--- asteroids.py
+++ asteroids.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python2
-import math
-import random
 import pyglet
 import utils
 from pyglet.window import key
@@ -12,88 +10,95 @@ HEIGHT = 600
 SCREEN_SIZE = [WIDTH, HEIGHT]
 DIMENSIONS = 2
 
-# Global varibles
-score = 0
-difficulty = score + 50
-lives = 3
-time = 0.5
-window = pyglet.window.Window(WIDTH, HEIGHT)
-ships = pyglet.graphics.Batch()
-rocks = pyglet.graphics.Batch()
-missiles = pyglet.graphics.Batch()
+class Window(pyglet.window.Window):
+    def __init__(self, *args, **kwargs):
+        super(Window, self).__init__(*args, **kwargs)
 
-# Images
-background = pyglet.image.load('static/images/nebula_blue.s2014.png')
+        self.score = 0
+        self.difficulty = self.score + 50
+        self.lives = 3
+        self.time = 0.5
+        self.ships = pyglet.graphics.Batch()
+        self.rocks = pyglet.graphics.Batch()
+        self.missiles = pyglet.graphics.Batch()
 
-rock_img = pyglet.image.load('static/images/asteroid_blue.png')
-utils.center_image_anchor(rock_img)
+        # Images
+        self.background = pyglet.image.load('static/images/nebula_blue.s2014.png')
 
-ship_sequence = pyglet.image.load('static/images/double_ship.png')
-ship_imgs = pyglet.image.ImageGrid(ship_sequence, 1, 2)
-utils.center_image_grid_anchors(ship_imgs)
+        rock_img = pyglet.image.load('static/images/asteroid_blue.png')
+        utils.center_image_anchor(rock_img)
 
-# Sounds
-thruster_snd = pyglet.media.load('static/sounds/rocket.ogg', streaming=False)
-explosion_snd = pyglet.media.load('static/sounds/explosion.ogg', streaming=False)
+        ship_sequence = pyglet.image.load('static/images/double_ship.png')
+        ship_imgs = pyglet.image.ImageGrid(ship_sequence, 1, 2)
+        utils.center_image_grid_anchors(ship_imgs)
 
-# Global Varibles that need to be moved into a class. 
-ship = PlayerSprite(ship_imgs, thruster_snd, 50, 100, 0, 0, 0, 35, ships, missiles)
-# Temporary 
-rock_position = utils.random_position(WIDTH, HEIGHT)
-# Temporary
-rock = MovingSprite(rock_img, sound=explosion_snd, diff=difficulty, radius=40, batch=rocks)
+        # Sounds
+        thruster_snd = pyglet.media.load('static/sounds/rocket.ogg', streaming=False)
+        explosion_snd = pyglet.media.load('static/sounds/explosion.ogg', streaming=False)
 
-def accel():
-    ship.thrusters = True
+        # Spites 
+        self.ship = PlayerSprite(ship_imgs, thruster_snd, 50, 100, 0, 0, 0, 35, self.ships, self.missiles)
+        # Temporary 
+        rock_position = utils.random_position(WIDTH, HEIGHT)
+        # Temporary
+        self.rock = MovingSprite(rock_img, sound=explosion_snd, diff=self.difficulty, radius=40, batch=self.rocks)
+
+        # Keymaps
+        self.key_downs = {key.UP:self.accel, key.LEFT:self.left, key.RIGHT:self.right, key.SPACE:self.fire}
+        self.key_ups = {key.UP:self.decel, key.LEFT:self.right, key.RIGHT:self.left}
+
+        pyglet.clock.schedule_interval(self.update, 1/60.0)  # update at 60Hz
+
+    def accel(self):
+        self.ship.thrusters = True
     
-def decel():
-    ship.thrusters = False
+    def decel(self):
+        self.ship.thrusters = False
 
-def left():
-    ship.angle_vel -= 5
+    def left(self):
+        self.ship.angle_vel -= 5
 
-def right():
-    ship.angle_vel += 5
+    def right(self):
+        self.ship.angle_vel += 5
     
-def fire():
-    ship.shoot()
-    
-key_downs = {key.UP:accel, key.LEFT:left, key.RIGHT:right, key.SPACE:fire}
-key_ups = {key.UP:decel,key.LEFT:right, key.RIGHT:left}
+    def fire(self):
+        self.ship.shoot()
 
-@window.event
-def on_key_press(symbol, modifiers):
-    for key in key_downs:
-        if key == symbol:
-            key_downs[symbol]()
+    def on_key_press(self, symbol, modifiers):
+        for key in self.key_downs:
+            if key == symbol:
+                self.key_downs[symbol]()
 
-@window.event
-def on_key_release(symbol, modifiers):
-    for key in key_ups:
-        if key == symbol:
-            key_ups[symbol]()
+    def on_key_release(self, symbol, modifiers):
+        for key in self.key_ups:
+            if key == symbol:
+                self.key_ups[symbol]()
 
-# TODO Implement Colisons
-# TODO Implement Sheilds
-# TODO Implement Spontaneos Rock Spawning
-# TODO Display Spash Screen
+    # TODO Implement Colisons
+    # TODO Implement Sheilds
+    # TODO Implement Spontaneos Rock Spawning
+    # TODO Display Spash Screen
+    def on_draw(self):
+        self.clear()
+        self.background.blit(0, 0)
+        self.ships.draw()
+        self.rocks.draw()
+        self.missiles.draw()
 
-@window.event
-def on_draw():
-    window.clear()
-    background.blit(0, 0)
-    ships.draw()
-    rocks.draw()
-    missiles.draw()
+    def update(self, dt):
+        self.rock.update(WIDTH, HEIGHT) 
+        self.ship.update(WIDTH, HEIGHT) 
+        self.local_missiles = set(self.ship.missiles_fired)
+        for missile in self.local_missiles:
+            if not missile.update(WIDTH, HEIGHT):
+                self.ship.missiles_fired.remove(missile)
+                missile.delete()
 
-def update(dt):
-    rock.update(WIDTH, HEIGHT) 
-    ship.update(WIDTH, HEIGHT) 
-    local_missiles = set(ship.missiles_fired)
-    for missile in local_missiles:
-        if not missile.update(WIDTH, HEIGHT):
-            ship.missiles_fired.remove(missile)
-            missile.delete()
 
-pyglet.clock.schedule_interval(update, 1/60.0)  # update at 60Hz
-pyglet.app.run()
+
+
+def main():
+    window = Window(WIDTH, HEIGHT)
+    pyglet.app.run()
+
+main()
